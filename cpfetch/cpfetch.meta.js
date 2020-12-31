e035dea418793b29018e5e12777e3780bd30c6f1
privet-kitty <privet-kitty@users.noreply.github.com>
2020-12-31 14:23:04 +0000

    deploy: d6f4e02207225f6023a282b696580610de62c59e

---
+++ cpfetch/cpfetch.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        cpfetch
+// @version     0.3.0
+// @author      Hugo Sansaqua
+// @description Userscript for competitive programming: parse test cases and copy template to clipboard on various online judges
+// @homepage    https://github.com/privet-kitty/cpfetch
+// @match       https://atcoder.jp/*/tasks/*
+// @match       https://*.atcoder.jp/tasks/*
+// @match       https://*.codechef.com/*/problems/*
+// @match       https://*.codechef.com/problems/*
+// @match       https://yukicoder.me/problems/*
+// @match       https://toph.co/p/*
+// @match       https://toph.co/arena*
+// @match       https://csacademy.com/contest/*/task/*
+// @namespace   https://privet-kitty.github.io/
+// @grant       GM_setClipboard
+// @grant       GM_registerMenuCommand
+// @grant       GM_getResourceText
+// @resource    template https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/non-module/template.lisp
+// @resource    modOperations https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/module/mod-operations.lisp
+// @resource    increaseStack https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/non-module/increase-space.lisp
+// ==/UserScript==
