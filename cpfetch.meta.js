e3f54dd0b28c0c161dc4bfcb016ce822e63563b6
privet-kitty <privet-kitty@users.noreply.github.com>
2021-01-01 21:53:25 +0000

    deploy: a3391a569b107df446f9b8110928567f4b3d2946

--- cpfetch.meta.js
+++ cpfetch.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        cpfetch
-// @version     0.3.2
+// @version     0.3.3
 // @author      Hugo Sansaqua
 // @description Userscript for competitive programming: parse test cases and copy template to clipboard on various online judges
 // @homepage    https://github.com/privet-kitty/cpfetch
