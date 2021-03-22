24b53f2dc594ee891e55c0668dd3246433f6be50
privet-kitty <privet.kitty99@gmail.com>
2021-03-23 08:40:02 +0900

    Add MojaCoder

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import { siteCodeChef } from './codechef';
 import { siteYukicoder } from './yukicoder';
 import { siteCsAcademy } from './csacademy';
 import { siteToph } from './toph';
+import { siteMojacoder } from './mojacoder';
 
 const hostname = new URL(document.URL).hostname;
 
-[siteAtCoder, siteCodeChef, siteYukicoder, siteCsAcademy, siteToph].forEach((site) => {
-  if (new RegExp(site.domain).test(hostname)) setUpSite(site);
-});
+[siteAtCoder, siteCodeChef, siteYukicoder, siteCsAcademy, siteToph, siteMojacoder].forEach(
+  (site) => {
+    if (new RegExp(site.domain).test(hostname)) setUpSite(site);
+  },
+);
---
+++ src/mojacoder.ts
@@ -0,0 +1,47 @@
+import { SiteObject } from './types';
+import { createCopyButton, prettify, zip } from './util';
+
+const findTestCases = (document: Document) => {
+  const inputs: string[] = [];
+  const outputs: string[] = [];
+  document.querySelectorAll('pre.form-control').forEach((node) => {
+    const textContent = node.textContent?.normalize('NFKC');
+    if (textContent === undefined) return;
+    const inputRegExp = /入力/;
+    const outputRegExp = /出力/;
+    const header = node.parentElement?.previousElementSibling?.textContent;
+    if (header === undefined || header === null) return;
+    if (inputRegExp.test(header)) {
+      inputs.push(prettify(textContent));
+    }
+    if (outputRegExp.test(header)) {
+      outputs.push(prettify(textContent));
+    }
+  });
+
+  if (inputs.length === outputs.length) {
+    return zip(inputs, outputs);
+  } else {
+    console.log('Failed to parse test cases.');
+    return [];
+  }
+};
+
+const appendCopyButton = (h3: Node | null, handler: () => void) => {
+  if (h3 === null) return false;
+  const copyButton = createCopyButton(handler);
+  h3.parentNode?.insertBefore(copyButton, h3.nextSibling);
+  return true;
+};
+
+export const siteMojacoder: SiteObject = {
+  domain: 'mojacoder.app',
+  findTestCases,
+  addCopyButton: (document: Document, handler: () => void) => {
+    document.querySelectorAll('h3').forEach((node) => {
+      if (node.textContent !== null && /問題文/.test(node.textContent)) {
+        appendCopyButton(node, handler);
+      }
+    });
+  },
+};
