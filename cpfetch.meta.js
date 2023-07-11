83a760ac5c3f1761320fa775d77749bb67aad6fa
privet-kitty <privet-kitty@users.noreply.github.com>
2023-07-11 01:35:16 +0000

    deploy: eb53f72a4cfe0f5af9dd9432617ef62e6aab2b47

--- cpfetch.meta.js
+++ cpfetch.meta.js
@@ -1,25 +1,25 @@
 // ==UserScript==
-// @name        cpfetch
-// @version     0.4.6
-// @author      Hugo Sansaqua
+// @name cpfetch
 // @description Userscript for competitive programming: parse test cases and copy template to clipboard on various online judges
-// @homepage    https://github.com/privet-kitty/cpfetch
-// @match       https://atcoder.jp/*/tasks/*
-// @match       https://*.codechef.com/*/problems/*
-// @match       https://*.codechef.com/problems/*
-// @match       https://yukicoder.me/problems/*
-// @match       https://toph.co/p/*
-// @match       https://toph.co/arena*
-// @match       https://csacademy.com/contest/*/task/*
-// @match       https://mojacoder.app/*/*/problems/*
-// @match       https://dmoj.ca/problem/*
-// @namespace   https://privet-kitty.github.io/
-// @grant       GM_setClipboard
-// @grant       GM_registerMenuCommand
-// @grant       GM_getResourceText
-// @resource    template https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/non-module/template.lisp
-// @resource    modOperations https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/module/mod-operations.lisp
-// @resource    increaseStack https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/non-module/increase-space.lisp
+// @version 0.4.6
+// @author Hugo Sansaqua
+// @homepage https://github.com/privet-kitty/cpfetch
+// @match https://atcoder.jp/*/tasks/*
+// @match https://*.codechef.com/*/problems/*
+// @match https://*.codechef.com/problems/*
+// @match https://yukicoder.me/problems/*
+// @match https://toph.co/p/*
+// @match https://toph.co/arena*
+// @match https://csacademy.com/contest/*/task/*
+// @match https://mojacoder.app/*/*/problems/*
+// @match https://dmoj.ca/problem/*
 // @downloadURL https://privet-kitty.github.io/cpfetch/cpfetch.user.js
-// @updateURL   https://privet-kitty.github.io/cpfetch/cpfetch.meta.js
+// @grant GM_setClipboard
+// @grant GM_registerMenuCommand
+// @grant GM_getResourceText
+// @namespace https://privet-kitty.github.io/
+// @resource template https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/non-module/template.lisp
+// @resource modOperation https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/module/mod-operations.lisp
+// @resource increaseStac https://raw.githubusercontent.com/privet-kitty/cl-competitive/master/non-module/increase-space.lisp
+// @updateURL https://privet-kitty.github.io/cpfetch/cpfetch.meta.js
 // ==/UserScript==
